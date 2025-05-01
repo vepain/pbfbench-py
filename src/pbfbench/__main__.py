999b7b92498d0a9cb15c69711045f8dc0b9d42db
vepain <victorepain@disroot.org>
2025-04-30 18:04:12 -0700

    feat: :sparkles: Init pbfbench application

---
+++ src/pbfbench/__main__.py
@@ -0,0 +1,12 @@
+"""PlasBin-flow benchmarking framework entry point."""
+
+from pbfbench.app import APP
+
+
+def main() -> None:
+    """Execute the main."""
+    APP()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pbfbench/app.py
@@ -0,0 +1,48 @@
+"""Root pbfbench application module."""
+
+# Due to typer usage:
+# ruff: noqa: TC001, TC003, UP007, FBT001, FBT002, PLR0913
+
+from __future__ import annotations
+
+from enum import StrEnum
+
+import typer
+
+import pbfbench.doc.app as doc_app
+import pbfbench.help.app as help_app
+import pbfbench.topics.seeds.app as seeds_app
+
+
+class PBFCommand:
+    """PBF command."""
+
+    NAME = "pbfbench"
+    HELP = "PlasBin-flow benchmarking framework"
+
+
+APP = typer.Typer(
+    name=PBFCommand.NAME,
+    help=PBFCommand.HELP,
+    rich_markup_mode="rich",
+)
+
+
+class CommandCategories(StrEnum):
+    """Command categories."""
+
+    UTILITIES = "Utilities"
+    TOPICS = "Topics"
+
+
+#
+# Utilities
+#
+for app in (doc_app.APP, help_app.APP):
+    APP.add_typer(app, rich_help_panel=CommandCategories.UTILITIES)
+
+#
+# Topics
+#
+for app in (seeds_app.APP,):
+    APP.add_typer(app, rich_help_panel=CommandCategories.TOPICS)
