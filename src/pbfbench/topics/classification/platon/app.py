799f62298d957a9e7fa21f2be187df34f7ca8ded
vepain <victorepain@disroot.org>
2025-09-24 12:27:27 +0200

    refactor: :truck: Short import

--- src/pbfbench/topics/classification/platon/app.py
+++ src/pbfbench/topics/classification/platon/app.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import pbfbench.abc.tool.app as abc_tool_app
-import pbfbench.topics.classification.platon.connector as platon_connector
 
-APP = abc_tool_app.build_application_with_arguments(platon_connector.Connector)
+from . import connector
+
+APP = abc_tool_app.build_application_with_arguments(connector.Connector)
